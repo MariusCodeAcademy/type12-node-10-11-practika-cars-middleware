552fff511ad5482fff28ab08f271bf19f1c65609
Marius <marius@example.com>
2022-09-02 11:42:01 +0300

    deleteCar

--- public/js/main.js
+++ public/js/main.js
@@ -6,11 +6,23 @@ console.log('main.js');
 const baseUrl = 'http://localhost:3000/api';
 const carListEl = document.getElementById('cars');
 
+function deleteCar(idToDelete) {
+  console.log('delete car', idToDelete);
+  // siusti delete uzklausa i back end
+  // pasitikrinam ar resp.ok arba resp.status
+  // jei ok tai atnaujinam sarasa
+  // jei ne pranesam apie klaida
+}
+
 function makeCarsListHtml(carsArr, dest) {
   dest.innerHTML = '';
   carsArr.forEach((cObj) => {
     const liEl = document.createElement('li');
     liEl.textContent = `${cObj.title} - $${cObj.price}`;
+    const buttonEl = document.createElement('button');
+    buttonEl.addEventListener('click', () => deleteCar(cObj.id));
+    buttonEl.textContent = 'delete';
+    liEl.append(buttonEl);
     dest.append(liEl);
   });
 }
