26501f1c42348ace461b5904475c54ad41e2030c
Marius <marius@example.com>
2022-09-02 10:39:47 +0300

    C:/Program Files/Git/api/cars/5

--- src/routes/carsRoutes.js
+++ src/routes/carsRoutes.js
@@ -26,4 +26,39 @@ carRouter.get('/', async (req, res) => {
   }
 });
 
+// GET /api/cars/5
+carRouter.get('/:carId', async (req, res) => {
+  const { carId } = req.params;
+  console.log('carId ===', carId);
+  console.log('typeof carId ===', typeof carId);
+  // patikrinimas ar carId yra skaicius
+  const idNum = Number(carId);
+  if (Number.isNaN(idNum)) {
+    res.status(400).json({ msg: 'car id shold be a number' });
+    return;
+  }
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    const sql = `SELECT ${selectedColumns} FROM cars WHERE deleted = 0 AND id = ?`;
+    console.log('sql ===', sql);
+    const [rows] = await conn.execute(sql, [carId]);
+    // jei radom viena irasa
+    if (rows.length === 1) {
+      res.status(200).json(rows[0]);
+    } else {
+      // jei neradom
+      res.status(404).json({ msg: 'not found' });
+    }
+
+    conn.end();
+  } catch (error) {
+    console.log('error ', error);
+    res.status(500).json({
+      msg: 'Something went wrong',
+    });
+  }
+});
+
+// 3. DELETE /api/cars/5 - istrina 1 auto pagal id arba 404
+
 module.exports = carRouter;
